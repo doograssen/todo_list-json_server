0b8a5a02248222979b4c9c0eda5c7b672062dc5f
Alexander <rythmag@gmail.com>
2024-05-06 17:47:13 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,53 @@
-// import logo from './logo.svg';
 import { useState, useEffect } from 'react';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
 import './App.css';
+
+const ERRORS = {
+	EMPTY: 'Поле обязательно для заполнения',
+	MIN_LENGTH: 'слишком короткое описание',
+	MAX_LENGTH: 'слишком длинное описание задачи',
+};
+
+const EditScheme = yup.object().shape({
+	edit: yup
+		.string()
+		.required(ERRORS.EMPTY)
+		.min(8, ERRORS.MIN_LENGTH)
+		.max(30, ERRORS.MAX_LENGTH),
+});
 
 export const App = () => {
 	const [TodoList, setTodoList] = useState([]);
 	const [isLoading, setIsLoading] = useState(false);
 	const [refreshTodoListFlag, setRefreshTodoListFlag] = useState(false);
 
+	// Form.
+
+	const onSubmit = (formData) => {
+		console.log(formData);
+	};
+
+	const {
+		register,
+		handleSubmit,
+		formState: { touchedFields, isValid, errors },
+	} = useForm({
+		defaultValues: {
+			edit: '',
+		},
+		resolver: yupResolver(EditScheme),
+		mode: 'onTouched',
+	});
+
+
+
+	// End of form
+
 	useEffect(() => {
 		setIsLoading(true);
-		fetch('https://jsonplaceholder.typicode.com/todos')
+		fetch('http://localhost:3005/todos')
 		.then((loadedData) => loadedData.json())
 		.then((loadedTodos) => {
 			setTodoList(loadedTodos);
@@ -23,8 +61,19 @@ export const App = () => {
 	return (
 		<div className="app">
 			<main className="app-main">
-				<h1 class="app-title">Team ToDo Example</h1>
+				<h1 className="app-title">Team ToDo Example</h1>
 				<div className="app-container">
+					<form className="app-form" onSubmit={handleSubmit(onSubmit)}>
+						<input
+							id="edit"
+							className="app-form-input"
+							type="text"
+							placeholder="Введите задачу"
+							{...register('edit')}
+						/>
+						{errors.edit && <div className="form-error">{errors.edit?.message}</div>}
+						<button className="app-form-submit" type="submit" disabled={!isValid}>Добавить</button>
+					</form>
 					<h3 className="app-caption">
 						Everyone
 					</h3>
