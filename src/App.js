d76bd67123b43e18b18d54aa1118df3a67bef2c3
Alexander <rythmag@gmail.com>
2024-05-08 17:06:56 +0300

    update

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import { useForm } from 'react-hook-form';
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
 import './App.css';
+import editImage from './images/icon-edit.svg';
+import deleteImage from './images/icon-delete.svg';
+import plusImage from './images/icon-plus.svg';
+import searchImage from './images/icon-search.svg';
 
 const ERRORS = {
 	EMPTY: 'Поле обязательно для заполнения',
@@ -96,8 +100,17 @@ export const App = () => {
 							className="app-form-submit"
 							type="submit"
 							disabled={!isValid || isCreated}
+							aria-label="Добавить"
 						>
-							Добавить
+							<img src={plusImage} alt="Добавить"/>
+						</button>
+						<button
+							className="app-form-search"
+							type="submit"
+							disabled={!isValid || isCreated}
+							aria-label="Поиск"
+						>
+							<img src={searchImage} alt="Найти"/>
 						</button>
 					</form>
 					<h3 className="app-caption">Everyone</h3>
@@ -109,6 +122,19 @@ export const App = () => {
 								<li className="app-item" key={'key_' + id}>
 									<button className="app-button" aria-label="Кнопка"></button>
 									{text}
+									<button className="app-burger" aria-label="Контекстное меню">
+										<span className="app-dot"></span>
+										<span className="app-dot"></span>
+										<span className="app-dot"></span>
+									</button>
+									<div className="app-panel">
+										<button className="app-edit" aria-label="Редактировать">
+											<img src={editImage} alt="Edit"/>
+										</button>
+										<button className="app-delete" aria-label="Удалить">
+											<img src={deleteImage} alt="Delete"/>
+										</button>
+									</div>
 								</li>
 							))}
 						</ul>
